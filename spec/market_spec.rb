24c449ef0d8aeac4d73e163e4c1a33111dda2da1
Ryan Loughry <thecoderanger5280@icloud.com>
2022-09-20 09:56:55 -0600

    write initialize btest block

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -5,12 +5,26 @@ require './lib/market'
 
 RSpec.describe Market do
   before(:each) do
-    item1 = Item.new({name: 'Peach', price: "$0.75"})
-    item2 = Item.new({name: 'Tomato', price: "$0.50"})
-    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
-    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
-    vendor1 = Vendor.new("Rocky Mountain Fresh")
-    vendor2 = Vendor.new("Ba-Nom-a-Nom")
-    market = Market.new("South Pearl Street Farmers Market")
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: "$0.50"})
+    @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    @vendor1 = Vendor.new("Rocky Mountain Fresh")
+    @vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    @market = Market.new("South Pearl Street Farmers Market")
+  end
+
+  describe '#initialize' do
+    it 'exists' do
+      expect(@market).to be_an_instance_of(Market)
+    end
+
+    it 'has a name' do
+      expect(@market.name).to eq("South Pearl Street Farmers Market")
+    end
+
+    it 'has vendors' do
+      expect(@market.vendors).to eq([])
+    end
   end
 end
