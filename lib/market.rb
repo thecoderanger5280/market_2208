446872e70dcfe119716a0ec07b5f12fbc2e17257
Ryan Loughry <thecoderanger5280@icloud.com>
2022-09-20 11:32:10 -0600

    implement code for total inventory mehtod

--- lib/market.rb
+++ lib/market.rb
@@ -18,4 +18,30 @@ class Market
   def vendors_that_sell(item)
     @vendors.find_all { |vendor| vendor.inventory[item] != 0 }
   end
+
+  def total_inventory
+    items = []
+    quantity = Hash.new(0)
+    inventory = {}
+    @vendors.each do |vendor|
+      # require 'pry'; binding.pry
+      items << vendor.inventory.keys
+      vendor.inventory.keys.each_with_index do |item, i|
+        # require 'pry'; binding.pry
+        quantity[item] += vendor.inventory.values[i]
+      end
+    end
+    # require 'pry'; binding.pry
+    unique_items = items.flatten.uniq
+    unique_items.each_with_index do |item, i|
+      # require 'pry'; binding.pry
+      vendors_with_item = @vendors.find_all do |vendor|
+        vendor.inventory.include?(item)
+      end
+      # require 'pry'; binding.pry
+      inventory[item] = {quantity: quantity[item], vendors: vendors_with_item}
+    end
+    # require 'pry'; binding.pry
+    inventory
+  end
 end
--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -73,7 +73,7 @@ RSpec.describe Market do
       @market.add_vendor(@vendor2)
       @market.add_vendor(@vendor3)
 
-      expect(@market.total_inventory).to eq({@item1 => {quantity: 100, vendors: [@vendor1, @vendor3]}, @item2, => {quantity: 7, vendors: [@vendor1]}, @item3 => {quantity: 50, vendors: [@vendor2]}, @item4 => {quantity: 35, vendors: [@vendor2, @vendor3]}})
+      expect(@market.total_inventory).to eq({@item1 => {quantity: 100, vendors: [@vendor1, @vendor3]}, @item2 => {quantity: 7, vendors: [@vendor1]}, @item3 => {quantity: 50, vendors: [@vendor2]}, @item4 => {quantity: 35, vendors: [@vendor2, @vendor3]}})
     end
   end
 
