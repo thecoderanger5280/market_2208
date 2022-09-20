491e07de3268d737499601dedcb0f37abff04f0c
Ryan Loughry <thecoderanger5280@icloud.com>
2022-09-20 09:24:18 -0600

    start check stock btest block

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -22,4 +22,10 @@ RSpec.describe Vendor do
       expect(@vendor.inventory).to eq({})
     end
   end
+
+  describe '#check_stock' do
+    it 'can check the stock of an item' do
+      expect(@vendor.check_stock(item1)).to eq(0)
+    end
+  end
 end
