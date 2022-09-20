84b03a45a6d55243b634eb05f31e137f1fd92816
Ryan Loughry <thecoderanger5280@icloud.com>
2022-09-20 10:20:01 -0600

    implement code for  vendor names mehtod

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -47,12 +47,20 @@ RSpec.describe Market do
 
   describe '#vendor_names' do
     it 'can list the names of the vendors' do
+      @market.add_vendor(@vendor1)
+      @market.add_vendor(@vendor2)
+      @market.add_vendor(@vendor3)
+
       expect(@market.vendor_names).to eq(["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"])
     end
   end
 
   describe '#vendors_that_sell' do
     it 'can list the vendors that sell a specific item' do
+      @market.add_vendor(@vendor1)
+      @market.add_vendor(@vendor2)
+      @market.add_vendor(@vendor3)
+
       expect(@market.vendors_that_sell(@item1)).to eq([@vendor1, @vendor3])
       expect(@market.vendors_that_sell(@item4)).to eq([@vendor2])
     end
