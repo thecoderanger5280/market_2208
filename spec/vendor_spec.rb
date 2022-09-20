c20c3a2f02b10e3df5022acd001b7faf654e340f
Ryan Loughry <thecoderanger5280@icloud.com>
2022-09-20 09:37:20 -0600

    finish test for stock mehtod

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -34,6 +34,18 @@ RSpec.describe Vendor do
       @vendor.stock(item1, 30)
 
       expect(@vendor.inventory).to eq({item1 => 30})
+      expect(@vendor.check_stock(item1)).to eq(30)
+
+      @vendor.stock(item1, 25)
+
+      expect(@vendor.check_stock(item1)).to eq(55)
+    end
+
+    it 'can stock multiple items' do
+      @vendor.stock(item1, 55)
+      @vendor.stock(item2, 12)
+
+      expect(@vendor.inventory).to eq({item1 => 55, item2 => 12})
     end
   end
 end
